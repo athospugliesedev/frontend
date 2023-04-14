d300a2615f258b7fb1235895e676025ffb88671a
Saulo Ferraz <ferrazsaulo1@gmail.com>
2023-04-13 23:44:00 -0300

    Commit-2
    Import e Alteração da Fonte

--- projeto01/src/App.jsx
+++ projeto01/src/App.jsx
@@ -12,23 +12,23 @@ function App() {
       <div className="w-full max-w-[1100px] flex items-center space-x-10 mt-8 ">
         <div className="max-w-[640px]">
           <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-            <strong className="text-2xl mb-6 block text-center">Cadastre-se gratuitamente</strong>
+            <strong className="text-2xl mb-6 block text-center font-abc">Cadastre-se gratuitamente</strong>
 
             <form onSubmit={handleClick} className="flex flex-col gap-2 w-full">
               <input
-                className="bg-gray-900 rounded px-5 h-14"
+                className="bg-gray-900 rounded px-5 h-14 font-abc"
                 type="text"
                 placeholder="Seu nome completo"
               />
               <input
-                className="bg-gray-900 rounded px-5 h-14"
+                className="bg-gray-900 rounded px-5 h-14 font-abc"
                 type="email"
                 placeholder="Digite seu e-mail"
               />
 
               <button
                 type="submit"
-                className="mt-4 bg-orange-500 uppercase py-4 rounded font-bold text-sm hover:bg-orange-700 transition-colors disabled:opacity-50"
+                className="mt-4 bg-orange-500 uppercase py-4 rounded font-bold text-sm hover:bg-orange-700 transition-colors disabled:opacity-50 font-abc"
                 onSubmit={handleClick}
               >
                 Entrar
@@ -40,10 +40,10 @@ function App() {
 
         </div>
         <div className="flex flex-col">
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
-            Música da <strong className="text-orange-500">rua</strong>, para quem é da <strong className="text-orange-600">rua</strong>.
+          <h1 className="mt-8 text-[2.5rem] leading-tight font-abc">
+            Música da <strong className="text-orange-500 font-abc">rua</strong>, para quem é da <strong className="text-orange-600 font-abc">rua</strong>.
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="mt-4 text-gray-200 leading-relaxed font-abc">
 
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin semper eleifend. Maecenas aliquam dui lorem, vitae congue magna bibendum sit amet. Sed diam justo, imperdiet nec eleifend non, egestas eget mi. Aenean eget condimentum massa. Nunc quis sem id felis imperdiet aliquam sed quis mauris.
           </p>
